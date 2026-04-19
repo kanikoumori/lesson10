080e7f5fe9324289f950091ab4eaf39b36643755
kouryou <miyata.01.pc@gmail.com>
2026-04-19 18:42:04 +0900

    Update

---
+++ python_beginner/app1.py
@@ -0,0 +1,13 @@
+#print(1+1)
+#print(5*10)
+#print(20/5)
+#print("あああ"+"いいい"+"aaa")
+#print("あああ"*3)
+#print('私は"おはよう"と言いました')
+#name="あああああ"
+#print("私の名前は"+name+"です")
+import random as rrr
+omikuzi=["大吉","中吉","小吉","末吉","凶","大凶"]
+print("今日のあなたの運勢は"+rrr.choice(omikuzi)+"です")
+
+
